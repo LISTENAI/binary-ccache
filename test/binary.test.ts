f089896b662b0ebbff8703150f66dffaf3e1f580
xychen <xychen@listenai.com>
2022-07-27 10:19:59 +0800

    Add functionality test

--- test/binary.test.ts
+++ test/binary.test.ts
@@ -1,5 +1,11 @@
-import { isAbsolute } from 'path';
+import { execFile as _execFile } from 'child_process';
+import { isAbsolute, join } from 'path';
+import { promisify } from 'util';
 import binary from '../src';
+
+const execFile = promisify(_execFile);
+
+const FIXTURES_DIR = join(__dirname, 'fixtures');
 
 test('homeDir', () => {
   expect(isAbsolute(binary.homeDir)).toBe(true);
@@ -12,3 +18,15 @@ test('binaryDir', () => {
 test('version()', async () => {
   expect(await binary.version()).toBeTruthy();
 });
+
+test('functionality', async () => {
+  await expect(execFile(join(binary.binaryDir, 'ccache'), [
+    'gcc',
+    join(FIXTURES_DIR, 'hello_world.c'),
+    '-o',
+    join(FIXTURES_DIR, 'hello_world.exe'),
+  ])).resolves.toBeTruthy();
+
+  const { stdout } = await execFile(join(FIXTURES_DIR, 'hello_world.exe'));
+  expect(stdout.trim()).toEqual('hello world!');
+}, 30 * 1000);
